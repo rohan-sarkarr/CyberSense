9ffdc6522ff5b407ee5615867aa0b699031d17f7
rohan-sarkarr <rohan180301@gmail.com>
2022-04-22 09:31:10 +0530

    backend added

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-/*$(document).ready(function(){
-	$(window).scroll(function(){
-		if(this.scrollY>20){
 
-			$('.navbar').css("background","#079DFF");
-
-			$('.navbar').addClass("sticky");
-
-		
-
-		}else{
-
-			$('.navbar').css("background","none");
-			$('.navbar').removeClass("sticky");
-		}
-	});
-});*/
-
-/*$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-});*/
-
-const header=document.querySelector('.navbar');
+const header = document.querySelector('.navbar');
 document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 20) {
-        header.style.backgroundColor = '#087bc6';
-    } else {
-        header.style.backgroundColor = 'transparent';
-    }
+  var scroll_position = window.scrollY;
+  if (scroll_position > 20) {
+    header.style.backgroundColor = '#087bc6';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
 });
 
-function rotateFunction(){
+function rotateFunction() {
   var min = 1024;
   var max = 9999;
   var deg = Math.floor(Math.random() * (max - min)) + min;
-  document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+  document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
 }
 
 var element = document.getElementById('mainbox');
 
-  element.classList.remove('animate');
+element.classList.remove('animate');
 
-  setTimeout(function(){
-    element.classList.add('animate');
-  }, 5000);
+setTimeout(function () {
+  element.classList.add('animate');
+}, 5000);
+
+$(document).ready(function () {
+
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+
+  });
+});
